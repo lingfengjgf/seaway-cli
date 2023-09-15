8510a63e71b4c05fce6bd361f41a71854eec36fc
lingfengjgf <lingfengjgf@163.com>
2023-09-15 20:52:35 +0800

    checkRoot\checkUserHome\checkEnv

--- seaway-cli/core/core/lib/index.js
+++ seaway-cli/core/core/lib/index.js
@@ -1,8 +1,11 @@
 "use strict";
 module.exports = core;
 
+const path = require("path");
 const semver = require("semver");
 const colors = require("colors");
+const userHome = require("user-home");
+const pathExists = require("path-exists").sync;
 const pkg = require("../package.json");
 const log = require("@seaway-cli/log");
 const constant = require("./const");
@@ -11,9 +14,47 @@ function core() {
   try {
     checkPkgVersion();
     checkNodeVersion();
+    checkRoot();
+    checkUserHome();
+    checkEnv();
   } catch (error) {
     log.error(error.message);
   }
+}
+
+function checkEnv() {
+  const dotenv = require("dotenv");
+  const dotenvPath = path.resolve(userHome, ".env");
+  if (pathExists(dotenvPath)) {
+    dotenv.config({
+      path: dotenvPath,
+    });
+  }
+  createDefaultConfig();
+  console.log(process.env.SEAWAY_CLI_HOME_PATH);
+}
+
+function createDefaultConfig() {
+  const cliConfig = {
+    home: userHome,
+  };
+  if (process.env.SEAWAY_CLI_HOME) {
+    cliConfig["cliHome"] = path.join(userHome, process.env.SEAWAY_CLI_HOME);
+  } else {
+    cliConfig["cliHome"] = path.join(userHome, constant.DEFAULT_CLI_HOME);
+  }
+  process.env.SEAWAY_CLI_HOME_PATH = cliConfig.cliHome;
+}
+
+function checkUserHome() {
+  if (!userHome || !pathExists(userHome)) {
+    throw new Error(colors.red("当前登录用户主目录不存在！"));
+  }
+}
+
+function checkRoot() {
+  const rootCheck = require("root-check");
+  rootCheck();
 }
 
 function checkNodeVersion() {
