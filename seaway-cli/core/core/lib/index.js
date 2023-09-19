773f36e933fc3415e324a06607dcb5d0d8f90fac
lingfengjgf <lingfengjgf@163.com>
2023-09-19 21:37:18 +0800

    registerCommand

--- seaway-cli/core/core/lib/index.js
+++ seaway-cli/core/core/lib/index.js
@@ -6,20 +6,78 @@ const semver = require("semver");
 const colors = require("colors");
 const userHome = require("user-home");
 const pathExists = require("path-exists").sync;
+const commander = require("commander");
 const pkg = require("../package.json");
 const log = require("@seaway-cli/log");
+const exec = require("@seaway-cli/exec");
 const constant = require("./const");
+
+const program = new commander.Command();
 
 async function core() {
   try {
-    checkPkgVersion();
     checkNodeVersion();
-    checkRoot();
-    checkUserHome();
-    checkEnv();
-    await checkGlobalUpdate();
+    await prepare();
+    registerCommand();
   } catch (error) {
     log.error(error.message);
+  }
+}
+
+// 脚手架启动
+async function prepare() {
+  checkPkgVersion();
+  checkRoot();
+  checkUserHome();
+  checkEnv();
+  await checkGlobalUpdate();
+}
+
+// 注册命令
+function registerCommand() {
+  program
+    .name(Object.keys(pkg.bin)[0])
+    .usage("<command> [options]")
+    .version(pkg.version)
+    .option("-d, --debug", "是否开启调试模式", false)
+    .option("-tp, --targetPath <targetPath>", "是否指定本地调试文件路径", "");
+
+  program
+    .command("init [projectName]")
+    .option("-f, --force", "是否强制初始化项目")
+    .action(exec);
+
+  const opts = program.opts();
+  // 开启debug模式
+  program.on("option:debug", function () {
+    if (opts.debug) {
+      process.env.LOG_LEVEL = "verbose";
+    } else {
+      process.env.LOG_LEVEL = "info";
+    }
+    log.level = process.env.LOG_LEVEL;
+  });
+
+  // 指定targetPath
+  program.on("option:targetPath", function () {
+    if (opts.targetPath) {
+      process.env.CLI_TARGET_PATH = opts.targetPath;
+    }
+  });
+  // 监听未知命令
+  program.on("command:*", function (obj) {
+    const availableCommands = program.commands.map((cmd) => cmd.name());
+    console.log(colors.red("未知的命令：" + obj[0]));
+    if (availableCommands && availableCommands.length) {
+      console.log(colors.yellow("可用命令：" + availableCommands.join(", ")));
+    }
+  });
+
+  program.parse(process.argv);
+
+  if (program.args && program.args.length < 1) {
+    program.outputHelp();
+    console.log();
   }
 }
 
@@ -47,7 +105,6 @@ function checkEnv() {
     });
   }
   createDefaultConfig();
-  console.log(process.env.SEAWAY_CLI_HOME_PATH);
 }
 
 function createDefaultConfig() {
